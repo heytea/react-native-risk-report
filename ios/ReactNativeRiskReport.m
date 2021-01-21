c3d5d7f86725f8561ecc4deed4b07b85fa131940
chris <chriszhou@163.com>
2021-01-21 18:07:17 +0800

    change app key

--- ios/ReactNativeRiskReport.m
+++ ios/ReactNativeRiskReport.m
@@ -13,15 +13,13 @@ RCT_EXPORT_METHOD(getDeviceToken:(NSString *)appKey
 {
     
     SecurityDevice *device = [[SecurityDevice alloc] init];
-    [device initDevice:SECURITY_APPKEY :^(int code)  {
+    [device initDevice:appKey :^(int code)  {
       if (10000 == code) {
-          SecurityDevice *device = [[SecurityDevice alloc]init];
           SecuritySession *session = [device getSession];
           int code = session.code;
           NSString *token = session.session;
           if (10000 == code) {
             // 成功
-            NSLog(@"device token =====  %@",token);
             resolve(token);
           }else{
               reject()
